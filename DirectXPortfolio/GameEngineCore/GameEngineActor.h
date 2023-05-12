feeaad9232b044735f1f939bf112159da0fb7130
kimtaemin <mini971018@gmail.com>
2023-05-12 15:55:30 +0900

    김태민_feat : 엔진 변경내용 추가

--- DirectXPortfolio/GameEngineCore/GameEngineActor.h
+++ DirectXPortfolio/GameEngineCore/GameEngineActor.h
@@ -17,11 +17,6 @@ public:
 	GameEngineActor& operator=(const GameEngineActor& _Other) = delete;
 	GameEngineActor& operator=(GameEngineActor&& _Other) noexcept = delete;
 
-	inline class GameEngineLevel* GetLevel() const
-	{
-		return Level;
-	}
-
 	template<typename ComponentType>
 	std::shared_ptr<ComponentType> CreateComponent()
 	{
@@ -37,21 +32,9 @@ protected:
 	virtual void Update(float _DeltaTime) {}
 	virtual void Render(float _DeltaTime) {}
 
-	void AccLiveTime(float _LiveTime) override;
-
-
 private:
-	class GameEngineLevel* Level = nullptr;
-
-	//// 이걸 컴포넌트 구조라고 합니다.
-	std::list<std::shared_ptr<class GameEngineComponent>> ComponentsList;
-
 	void ComponentInit(std::shared_ptr<class GameEngineComponent> _Component);
 
-	void ComponentsUpdate(float _DeltaTime);
-
-	void ComponentsRender(float _DeltaTime);
-
-	void ComponentsRelease();
+	
 };
 
--- DirectXPortfolio/GameEngineCore/GameEngineTransform.cpp
+++ DirectXPortfolio/GameEngineCore/GameEngineTransform.cpp
@@ -1,6 +1,8 @@
 #include "PrecompileHeader.h"
 #include "GameEngineTransform.h"
 #include "GameEngineObject.h"
+#include "GameEngineActor.h"
+#include "GameEngineLevel.h"
 
 GameEngineTransform::GameEngineTransform()
 {
@@ -30,46 +32,50 @@ void GameEngineTransform::TransformUpdate()
 	}
 	else // 차이
 	{
-		float4x4 ParentWorldMatrix = Parent->GetWorldMatrixRef();
-		float4 PScale, PRotation, PPosition;
-		ParentWorldMatrix.Decompose(PScale, PRotation, PPosition);
-
-
-		if (true == AbsoluteScale)
-		{
-			// 부모쪽 행렬의 스케일을 1
-			PScale = float4::One;
-		}
-		if (true == AbsoluteRotation)
-		{
-			// 부모의 회전 
-			PRotation = float4::Zero;
-			PRotation.EulerDegToQuaternion();
-		}
-		if (true == AbsolutePosition)
-		{
-			PPosition = float4::Zero;
-		}
-
-		float4x4 MatScale, MatRot, MatPos;
-
-		//scale
-		MatScale.Scale(PScale);
-
-		//rot
-		MatRot = PRotation.QuaternionToRotationMatrix();
-
-		//pos
-		MatPos.Pos(PPosition);
-
-		TransData.WorldMatrix = TransData.LocalWorldMatrix * (MatScale * MatRot * MatPos);
+		WorldCalculation();
 	}
 
 	WorldDecompose();
-
 	LocalDecompose();
 		// ParentWorldMatrix.Decompose(PScale, PRoatation, PPosition);
 
+}
+
+void GameEngineTransform::WorldCalculation()
+{
+	float4x4 ParentWorldMatrix = Parent->GetWorldMatrixRef();
+	float4 PScale, PRotation, PPosition;
+	ParentWorldMatrix.Decompose(PScale, PRotation, PPosition);
+
+
+	if (true == AbsoluteScale)
+	{
+		// 부모쪽 행렬의 스케일을 1
+		PScale = float4::One;
+	}
+	if (true == AbsoluteRotation)
+	{
+		// 부모의 회전 
+		PRotation = float4::Zero;
+		PRotation.EulerDegToQuaternion();
+	}
+	if (true == AbsolutePosition)
+	{
+		PPosition = float4::Zero;
+	}
+
+	float4x4 MatScale, MatRot, MatPos;
+
+	//scale
+	MatScale.Scale(PScale);
+
+	//rot
+	MatRot = PRotation.QuaternionToRotationMatrix();
+
+	//pos
+	MatPos.Pos(PPosition);
+
+	TransData.WorldMatrix = TransData.LocalWorldMatrix * (MatScale * MatRot * MatPos);
 }
 
 void GameEngineTransform::LocalDecompose() 
@@ -92,6 +98,45 @@ void GameEngineTransform::SetParent(GameEngineTransform* _Parent)
 		int a = 0;
 	}
 
+	if (nullptr == _Parent && nullptr == Master)
+	{
+		if (nullptr == dynamic_cast<GameEngineActor*>(Master))
+		{
+			MsgAssert("액터만이 루트 리스트에 추가될수 있습니다.");
+			return;
+		}
+	}
+
+	if (nullptr == Parent && nullptr == _Parent)
+	{
+		return;
+	}
+
+	// 내가 원래 기존의 부모를 가지고 있다면
+	if (nullptr != Parent)
+	{
+		Parent->Child.remove(this);
+		GameEngineObject* ParentMaster = Parent->Master;
+
+		if (nullptr == ParentMaster)
+		{
+			MsgAssert("존재할수 없는 상황입니다 Master가 nullptr입니다");
+			return;
+		}
+
+		std::shared_ptr<GameEngineObject> MasterPtr = Master->shared_from_this();
+		ParentMaster->Childs.remove(MasterPtr);
+		Parent = nullptr;
+
+		GameEngineLevel* Level = Master->GetLevel();
+
+		Level->Actors[MasterPtr->GetOrder()].push_back(std::dynamic_pointer_cast<GameEngineActor>(MasterPtr));
+	}
+
+
+
+
+
 	Parent = _Parent;
 
 	// 월드 포지션은 달라지는게 없다.
@@ -106,22 +151,36 @@ void GameEngineTransform::SetParent(GameEngineTransform* _Parent)
 	//ChildData.WorldMatrix;
 	//float4x4 NewWorld = ChildData.WorldMatrix * ParentData.WorldMatrix.InverseReturn();
 
-	TransData.LocalWorldMatrix = TransData.WorldMatrix * Parent->TransData.WorldMatrix.InverseReturn();
-	LocalDecompose();
+	if (nullptr != Parent)
+	{
+		TransData.LocalWorldMatrix = TransData.WorldMatrix * Parent->TransData.WorldMatrix.InverseReturn();
 
-	TransData.Position = TransData.LocalPosition;
-	TransData.Rotation = TransData.LocalRotation;
-	TransData.Scale = TransData.LocalScale;
+		LocalDecompose();
 
-	TransformUpdate();
+		TransData.Position = TransData.LocalPosition;
+		TransData.Rotation = TransData.LocalRotation;
+		TransData.Scale = TransData.LocalScale;
 
-	AbsoluteReset();
+		TransformUpdate();
 
-	// 나의 로컬포지션 나의 로컬 이런것들이 있었는데.
-	// 나는 새로운 부모가 생겼고
-	// 내가 이미 다른 부모가 있다면
+		AbsoluteReset();
 
-	Parent->Child.push_back(this);
+		// 나의 로컬포지션 나의 로컬 이런것들이 있었는데.
+		// 나는 새로운 부모가 생겼고
+		// 내가 이미 다른 부모가 있다면
+
+		Parent->Child.push_back(this);
+	}
+	else 
+	{
+		WorldDecompose();
+
+		TransData.Position = TransData.WorldPosition;
+		TransData.Rotation = TransData.WorldRotation;
+		TransData.Scale = TransData.WorldScale;
+		TransformUpdate();
+		AbsoluteReset();
+	}
 }
 
 
@@ -131,20 +190,9 @@ void GameEngineTransform::CalChild()
 {
 	for (GameEngineTransform* ChildTrans : Child)
 	{
-		if (false == ChildTrans->AbsoluteScale)
-		{
-			ChildTrans->SetLocalScale(ChildTrans->GetLocalScale());
-		}
-
-		if (false == ChildTrans->AbsoluteRotation)
-		{
-			ChildTrans->SetLocalRotation(ChildTrans->GetLocalRotation());
-		}
-
-		if (false == ChildTrans->AbsolutePosition)
-		{
-			ChildTrans->SetLocalPosition(ChildTrans->GetLocalPosition());
-		}
+		ChildTrans->WorldCalculation();
+		ChildTrans->WorldDecompose();
+		ChildTrans->CalChild();
 	}
 }
 
@@ -185,4 +233,115 @@ void GameEngineTransform::AbsoluteReset()
 	AbsoluteScale = false;
 	AbsoluteRotation = false;
 	AbsolutePosition = false;
+}
+
+void GameEngineTransform::SetMaster(GameEngineObject* _Master)
+{
+	Master = _Master;
+}
+
+void GameEngineTransform::AllAccTime(float _DeltaTime)
+{
+	if (nullptr == Master)
+	{
+		return;
+	}
+
+	if (false == Master->IsUpdate())
+	{
+		return;
+	}
+
+	Master->AccLiveTime(_DeltaTime);
+
+	for (GameEngineTransform* Trans : Child)
+	{
+		Trans->AllAccTime(_DeltaTime);
+	}
+}
+
+void GameEngineTransform::AllUpdate(float _DeltaTime) 
+{
+
+	if (nullptr == Master)
+	{
+		return;
+	}
+
+	if (false == Master->IsUpdate())
+	{
+		return;
+	}
+
+	Master->Update(_DeltaTime);
+
+	for (GameEngineTransform* Trans : Child)
+	{
+		Trans->AllUpdate(_DeltaTime);
+	}
+}
+
+
+void GameEngineTransform::AllRender(float _DeltaTime) 
+{
+	if (nullptr == Master)
+	{
+		return;
+	}
+
+	if (false == Master->IsUpdate())
+	{
+		return;
+	}
+
+	Master->Render(_DeltaTime);
+
+	for (GameEngineTransform* Trans : Child)
+	{
+		Trans->AllRender(_DeltaTime);
+	}
+}
+
+void GameEngineTransform::AllRelease()
+{
+	if (nullptr == Master)
+	{
+		return;
+	}
+
+	if (false == Master->IsUpdate())
+	{
+		return;
+	}
+
+	Master->Release();
+
+	for (GameEngineTransform* Trans : Child)
+	{
+		Trans->AllRelease();
+	}
+}
+
+void GameEngineTransform::ChildRelease()
+{
+	std::list<GameEngineTransform*>::iterator ReleaseStartIter = Child.begin();
+	std::list<GameEngineTransform*>::iterator ReleaseEndIter = Child.end();
+
+	for (; ReleaseStartIter != ReleaseEndIter; )
+	{
+		GameEngineTransform* Trans = *ReleaseStartIter;
+
+		if (nullptr == Trans->Master)
+		{
+			MsgAssert("몬가 잘못됨 도라에몽을 부르자.");
+		}
+
+		if (false == Trans->Master->IsDeath())
+		{
+			++ReleaseStartIter;
+			continue;
+		}
+
+		ReleaseStartIter = Child.erase(ReleaseStartIter);
+	}
 }
